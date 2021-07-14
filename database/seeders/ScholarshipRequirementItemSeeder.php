3e7e13578b7e0bb68c9f875ff7c42bb83ee6186a
RhannelD <rhanneldinlasan@gmail.com>
2021-07-14 15:12:36 +0800

    Update Requirement Item Seeder

--- database/seeders/ScholarshipRequirementItemSeeder.php
+++ database/seeders/ScholarshipRequirementItemSeeder.php
@@ -18,10 +18,26 @@ class ScholarshipRequirementItemSeeder extends Seeder
         $requirements = ScholarshipRequirement::all();
 
         foreach ($requirements as $requirement) {
+            $item_COR = new ScholarshipRequirementItem;
+            $item_COR->requirement_id  = $requirement->id;
+            $item_COR->item = 'Certificate of Registration';
+            $item_COR->note = 'Upload in PDF file';
+            $item_COR->type = 'cor';
+            $item_COR->position = 1;
+            $item_COR->save();
+            
+            $item_Grade = new ScholarshipRequirementItem;
+            $item_Grade->requirement_id  = $requirement->id;
+            $item_Grade->item = 'Previuos Semester Grades';
+            $item_Grade->note = 'Upload in PDF file';
+            $item_Grade->type = 'grade';
+            $item_Grade->position = 2;
+            $item_Grade->save();
+
             for ($items=0; $items < rand(5,8); $items++) { 
                 ScholarshipRequirementItem::factory()->create([   
                     'requirement_id' => $requirement->id,
-                    'position' => ($items+1)
+                    'position' => ($items+3)
                 ]);
             }
         }
