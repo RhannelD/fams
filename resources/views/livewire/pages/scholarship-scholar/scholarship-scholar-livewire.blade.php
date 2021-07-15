f62727808dd93368012b07f9e7e519efe656fb64
RhannelD <rhanneldinlasan@gmail.com>
2021-07-16 00:35:55 +0800

    Edit Scholar view on Scholarship Scholar

--- resources/views/livewire/pages/scholarship-scholar/scholarship-scholar-livewire.blade.php
+++ resources/views/livewire/pages/scholarship-scholar/scholarship-scholar-livewire.blade.php
@@ -11,40 +11,42 @@
 
 		</div>
 
-        <div class="col-md-7 mt-2">
-            <div class="form-row">
-                <div class="input-group col-6 my-0">
-                    <div class="input-group-prepend">
-                      <label class="input-group-text" for="category">Category</label>
+        @if (!Auth::user()->usertype == 'scholar')
+            <div class="col-md-7 mt-2">
+                <div class="form-row">
+                    <div class="input-group col-6 my-0">
+                        <div class="input-group-prepend">
+                        <label class="input-group-text" for="category">Category</label>
+                        </div>
+                        <select wire:model="category_id" class="form-control" id="category">
+                            <option value="">All Categories</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category['id'] }}">{{ $category['category'] }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                    <select wire:model="category_id" class="form-control" id="category">
-                        <option value="">All Categories</option>
-                        @foreach ($categories as $category)
-                            <option value="{{ $category['id'] }}">{{ $category['category'] }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="input-group col-4 my-0">
-                    <div class="input-group-prepend">
-                      <label class="input-group-text" for="rows">Rows</label>
+                    <div class="input-group col-4 my-0">
+                        <div class="input-group-prepend">
+                        <label class="input-group-text" for="rows">Rows</label>
+                        </div>
+                        <select wire:model="show_row" class="form-control" id="rows">
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                            <option value="150">150</option>
+                            <option value="200">200</option>
+                        </select>
                     </div>
-                    <select wire:model="show_row" class="form-control" id="rows">
-                        <option value="10">10</option>
-                        <option value="15">15</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                        <option value="150">150</option>
-                        <option value="200">200</option>
-                    </select>
-                </div>
-                <div class="form-group col-2  my-0">
-                    <button class="form-control btn btn-success">
-                        Invite
-                    </button>
+                    <div class="form-group col-2  my-0">
+                        <button class="form-control btn btn-success">
+                            Invite
+                        </button>
+                    </div>
                 </div>
             </div>
-		</div>
+        @endif
 	</div>
 
 	<div class="row">
--- resources/views/livewire/pages/scholarship-scholar/scholarship-scholar-search-livewire.blade.php
+++ resources/views/livewire/pages/scholarship-scholar/scholarship-scholar-search-livewire.blade.php
@@ -7,110 +7,130 @@
         <table class="table myaccordion table-hover" id="accordion">
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    @empty($category_id)
-                        <th>Category</th>
-                    @endempty
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th></th>
+                    @if (!Auth::user()->usertype == 'scholar')
+                        <th>ID</th>
+                        <th>Name</th>
+                        @empty($category_id)
+                            <th>Category</th>
+                        @endempty
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th></th>
+                    @else  
+                        <th>Name</th>
+                        <th>Email</th>
+                    @endif
                 </tr>
             </thead>
             <tbody>
                 @forelse ($scholars as $scholar)
-                    <tr data-toggle="collapse" data-target="#collapse{{ $scholar->user_id }}" aria-expanded="true" aria-controls="collapse{{ $scholar->user_id }}" aria-expanded="false">
-                        <th scope="row">{{ $scholar->user_id }}</th>
-                        <td>{{ $scholar->firstname }} {{ $scholar->middlename }} {{ $scholar->lastname }}</td>
-                        @empty($category_id)
-                            <td>{{ $scholar->category }}</td>
-                        @endempty
-                        <td>{{ $scholar->email }}</td>
-                        <td>{{ $scholar->phone }}</td>
-                        <td>
-                            <i class="fa" aria-hidden="true"></i>
-                        </td>
+                    <tr 
+                        @if (!Auth::user()->usertype == 'scholar')    
+                            data-toggle="collapse" 
+                            data-target="#collapse{{ $scholar->user_id }}" 
+                            aria-expanded="true" 
+                            aria-controls="collapse{{ $scholar->user_id }}" 
+                            aria-expanded="false"
+                        @endif
+                        >
+                        @if (!Auth::user()->usertype == 'scholar')
+                            <th scope="row">{{ $scholar->user_id }}</th>
+                            <td>{{ $scholar->firstname }} {{ $scholar->middlename }} {{ $scholar->lastname }}</td>
+                            @empty($category_id)
+                                <td>{{ $scholar->category }}</td>
+                            @endempty
+                            <td>{{ $scholar->email }}</td>
+                            <td>{{ $scholar->phone }}</td>
+                            <td>
+                                <i class="fa" aria-hidden="true"></i>
+                            </td>
+                        @else  
+                            <td>{{ $scholar->firstname }} {{ $scholar->middlename }} {{ $scholar->lastname }}</td>
+                            <td>{{ $scholar->email }}</td>
+                        @endif
                     </tr>
-                    <tr>
-                        <td colspan="6" id="collapse{{ $scholar->user_id }}" data-parent="#accordion" class="collapse acc p-1" >
-                            <div class="card mb-3 shadow-sm ">
-                                <div class="card-body p-2">
-                                    <div class="row">
-                                        <div class="col-md-6 pb-0">
-                                            <table class="table table-borderless table-sm m-0">
-                                                <thead>
-                                                    <tr>
-                                                        <th colspan="2">
-                                                            Scholar Info
-                                                        </th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>ID:</td>
-                                                        <td>{{ $scholar->user_id }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Full Name:</td>
-                                                        <td>{{ $scholar->firstname }} {{ $scholar->middlename }} {{ $scholar->lastname }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Phonenumber:</td>
-                                                        <td>{{ $scholar->phone }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Email:</td>
-                                                        <td>{{ $scholar->email }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Gender:</td>
-                                                        <td>{{ $scholar->gender }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Religion:</td>
-                                                        <td>{{ $scholar->religion }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Birth Date:</td>
-                                                        <td>{{ $scholar->birthday }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Birth Place:</td>
-                                                        <td>{{ $scholar->birthplace }}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        <div class="col-md-6 pb-0">
-                                            <table class="table table-borderless table-sm m-0">
-                                                <thead>
-                                                    <tr>
-                                                        <th colspan="2">
-                                                            Scholarship
-                                                        </th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>Category:</td>
-                                                        <td>{{ $scholar->category }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Amount:</td>
-                                                        <td>{{ $scholar->amount }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Date Joined:</td>
-                                                        <td>{{ date_format(new DateTime($scholar->created_at),"M d, Y") }}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
+                    @if (!Auth::user()->usertype == 'scholar')
+                        <tr>
+                            <td colspan="6" id="collapse{{ $scholar->user_id }}" data-parent="#accordion" class="collapse acc p-1" >
+                                <div class="card mb-3 shadow-sm ">
+                                    <div class="card-body p-2">
+                                        <div class="row">
+                                            <div class="col-md-6 pb-0">
+                                                <table class="table table-borderless table-sm m-0">
+                                                    <thead>
+                                                        <tr>
+                                                            <th colspan="2">
+                                                                Scholar Info
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>ID:</td>
+                                                            <td>{{ $scholar->user_id }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Full Name:</td>
+                                                            <td>{{ $scholar->firstname }} {{ $scholar->middlename }} {{ $scholar->lastname }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Phonenumber:</td>
+                                                            <td>{{ $scholar->phone }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Email:</td>
+                                                            <td>{{ $scholar->email }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Gender:</td>
+                                                            <td>{{ $scholar->gender }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Religion:</td>
+                                                            <td>{{ $scholar->religion }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Birth Date:</td>
+                                                            <td>{{ $scholar->birthday }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Birth Place:</td>
+                                                            <td>{{ $scholar->birthplace }}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div class="col-md-6 pb-0">
+                                                <table class="table table-borderless table-sm m-0">
+                                                    <thead>
+                                                        <tr>
+                                                            <th colspan="2">
+                                                                Scholarship
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Category:</td>
+                                                            <td>{{ $scholar->category }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Amount:</td>
+                                                            <td>{{ $scholar->amount }}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Date Joined:</td>
+                                                            <td>{{ date_format(new DateTime($scholar->created_at),"M d, Y") }}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    @endif
                 @empty
                     <tr>
                         <td colspan="6">No results...</td>
