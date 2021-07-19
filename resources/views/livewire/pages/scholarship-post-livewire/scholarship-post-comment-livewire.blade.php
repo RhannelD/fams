adfab50af9ab3ad761c070d6371881b4689577ff
RhannelD <rhanneldinlasan@gmail.com>
2021-07-19 11:56:49 +0800

    minor refactor form on comment form

--- resources/views/livewire/pages/scholarship-post-livewire/scholarship-post-comment-livewire.blade.php
+++ resources/views/livewire/pages/scholarship-post-livewire/scholarship-post-comment-livewire.blade.php
@@ -4,15 +4,15 @@
             {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}
         </strong>
     </label>
-    <div class="form-group my-0">
+    <form class="form-group my-0" wire:submit.prevent="comment">
         <div class="d-flex">
             <textarea wire:model.lazy="comment.comment" class="form-control rounded" rows="1" placeholder="Comment something..."></textarea>
             <div>
-                <button wire:click="comment" class="btn btn-dark rounded ml-2" type="button">
+                <button class="btn btn-dark rounded ml-2" type="submit">
                     <i class="fas fa-edit"></i>
                 </button>
             </div>
         </div>
         @error('comment.comment') <span class="text-danger">{{ $message }}</span> @enderror
-    </div>
+    </form>
 </div>
