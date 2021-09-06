44534e51c94b36c982d1ef4795e0e5f3e7d04a5f
RhannelD <rhanneldinlasan@gmail.com>
2021-09-06 17:07:56 +0800

    move scho.requirement&open components to folders

--- app/Http/Livewire/ScholarshipRequirementResponse/ScholarshipRequirementResponseViewLivewire.php
+++ app/Http/Livewire/ScholarshipRequirementResponse/ScholarshipRequirementResponseViewLivewire.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\ScholarshipRequirementResponse;
 
 use Livewire\Component;
 use App\Models\ScholarResponse;
--- resources/views/livewire/pages/scholarship-requirement-response/scholarship-requirement-response-livewire.blade.php
+++ resources/views/livewire/pages/scholarship-requirement-response/scholarship-requirement-response-livewire.blade.php
@@ -124,7 +124,7 @@
 
                 <hr class="my-1 mx-3">
                 @isset($responses[$index]->id)
-                    @livewire('scholarship-requirement-response-view-livewire', [$responses[$index]->id], key('response-view-'.time().$requirement->scholarship_id))
+                    @livewire('scholarship-requirement-response.scholarship-requirement-response-view-livewire', [$responses[$index]->id], key('response-view-'.time().$requirement->scholarship_id))
                 @endisset  
             </div>
             @endisset
