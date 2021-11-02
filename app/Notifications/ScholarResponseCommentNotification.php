e6cae70ff98534f1bda9f6aa2181ddc40cb0453b
RhannelD <rhanneldinlasan@gmail.com>
2021-11-02 22:59:46 +0800

    add queue jobs for post and response comment

--- app/Notifications/ScholarResponseCommentNotification.php
+++ app/Notifications/ScholarResponseCommentNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ScholarResponseCommentNotification extends Notification
+class ScholarResponseCommentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
